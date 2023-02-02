b1209d6beabfd074d2810b9b22b13994b97c6ddb
mikalfoster <mikalfoster36@gmail.com>
2023-02-02 17:02:36 -0600

    commit tree b5bd042ef203a2a71329b83cee891ec01b371f00

---
+++ index.js
@@ -0,0 +1,26 @@
+function shout(string) {
+    return string.toUpperCase();
+};
+function whisper(string) {
+    return string.toLowerCase();
+};
+function logShout(string) {
+    console.log(`HELLO`);
+};
+function logWhisper(string) {
+    console.log(`hello`);
+};
+function sayHiToHeadphonedRoommate(string) {
+    return string === `Lets have dinner together!`;
+};
+function sayHiToHeadphonedRoommate(string) {
+    if (string === string.toLowerCase()) { 
+        return `I can't hear you!`;
+    } 
+    if (string === string.toUpperCase()) {
+        return `YES INDEED!`;
+    }
+    if (string === string) {
+        return `I would love to!`;
+    }
+}    
